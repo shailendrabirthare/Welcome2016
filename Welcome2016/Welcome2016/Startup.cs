4641ed962ce8cf98187e22e5495503f77c60b787
Shailendra Birthare <shailendra.birthare@gmail.com>
2016-01-07 10:28:51 +0530

    First check in with template code

---
+++ Welcome2016/Welcome2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Welcome2016.Startup))]
+namespace Welcome2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
